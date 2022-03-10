a02f7ab2993d7c7ad8258cbf17296127fffba7c1
luis <luis.passos013@gmail.com>
2022-03-09 22:19:13 -0300

    organized itens with flex in login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,30 @@
-import { Box, Flex } from "@chakra-ui/react";
-import type { NextPage } from "next";
-import { Header } from "../components/Header";
+import { Flex, Img, Link } from '@chakra-ui/react';
+import type { NextPage } from 'next';
+import { DividerOr } from '../components/Form/DividerOr';
+import { FormWrapper } from '../components/Form/FormWrapper';
+import { Input } from '../components/Form/Input';
+import { LoginButtonWithGoogle } from '../components/Form/LoginButtonWithGoogle';
+import { Header } from '../components/Header';
+import NextLink from 'next/link';
+import { Button } from '../components/Form/Button';
 
 const Login: NextPage = () => {
   return (
-    // <>
-    //   <Header />
-    //   <Flex px="10" gap="10" align="center">
-    //     <Img h="350px" src="/images/man_entering_img.svg" alt="Ilustração de login" />
-    //     <FormWrapper>
-    //       <LoginButtonWithGoogle />
-    //       <DividerOr />
-    //       <Input type="email" id="email" label="Email" name="email" />
-    //       <Input type="password" id="password" label="Senha" name="password" />
-    //       <NextLink href="/esqueci-minha-senha" passHref>
-    //         <Link>Esqueci minha senha</Link>
-    //       </NextLink>
-    //       <Button text="Entrar" />
-    //     </FormWrapper>
-    //   </Flex>
-    // </>
-    <Flex h="100vh" direction="column">
+    <Flex h='100vh' direction='column'>
       <Header />
-      <Box flex="1" bg="green.200"></Box>
+      <Flex px='10' gap='10' align='center' flex='1'>
+        <Img h='350px' src='/images/man_entering_img.svg' alt='Ilustração de login' />
+        <FormWrapper>
+          <LoginButtonWithGoogle />
+          <DividerOr />
+          <Input type='email' id='email' label='Email' name='email' />
+          <Input type='password' id='password' label='Senha' name='password' />
+          <NextLink href='/esqueci-minha-senha' passHref>
+            <Link>Esqueci minha senha</Link>
+          </NextLink>
+          <Button text='Entrar' />
+        </FormWrapper>
+      </Flex>
     </Flex>
   );
 };
