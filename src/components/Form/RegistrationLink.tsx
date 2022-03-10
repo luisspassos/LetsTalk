0c66b27bb5fe06fbf4357d59eacca8da3b2fef83
Luís Passos <luis.passos013@gmail.com>
2022-03-10 16:16:30 -0300

    add functional registration link

--- src/components/Form/RegistrationLink.tsx
+++ src/components/Form/RegistrationLink.tsx
@@ -3,10 +3,10 @@ import NextLink from "next/link";
 
 export function RegistrationLink() {
   return (
-    <Text align='center' mt='10px'>
+    <Text align='center' mt='10px' color='gray.400'>
       Não tem uma conta?{" "}
       <NextLink href='/cadastro' passHref>
-        <Link>Cadastre-se!</Link>
+        <Link color='blue.900'>Cadastre-se!</Link>
       </NextLink>
     </Text>
   );
