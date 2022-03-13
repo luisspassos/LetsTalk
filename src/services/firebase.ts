aa193bf27d064afbe9b330530bfefb856b03ae27
Luís Passos <luis.passos013@gmail.com>
2022-03-13 10:59:59 -0300

    added firebase sdk admin startup configuration (NOTEBOOK)

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,17 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { applicationDefault, initializeApp } from 'firebase-admin/app';
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
-
-initializeApp(firebaseConfig);
-
-const auth = getAuth();
-
-export { auth };
+initializeApp({
+  credential: applicationDefault(),
+});
