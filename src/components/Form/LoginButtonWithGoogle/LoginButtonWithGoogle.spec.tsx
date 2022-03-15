c54f17147f2ee4d57397dd20c02eba6b32ac094a
Luís Passos <luis.passos013@gmail.com>
2022-03-15 15:20:30 -0300

    add mock of signInWithPopup and Router function on login page

--- src/components/Form/LoginButtonWithGoogle/LoginButtonWithGoogle.spec.tsx
+++ src/components/Form/LoginButtonWithGoogle/LoginButtonWithGoogle.spec.tsx
@@ -1,5 +1,21 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import { useRouter } from 'next/router';
 import { LoginButtonWithGoogle } from '.';
+import { mocked } from 'jest-mock';
+
+jest.mock('next/router');
+
+jest.mock('firebase/auth', () => {
+  return {
+    signInWithPopup() {
+      return {
+        user: {
+          uid: 'fake-uid',
+        },
+      };
+    },
+  };
+});
 
 describe('LoginButtonWithGoogle component', () => {
   it('renders correctly', () => {
@@ -9,10 +25,19 @@ describe('LoginButtonWithGoogle component', () => {
   });
 
   it('redirects to the conversations page if the user is logged in', () => {
+    const useRouterMocked = mocked(useRouter);
+    const pushMock = jest.fn();
+
+    useRouterMocked.mockReturnValueOnce({
+      push: pushMock,
+    } as any);
+
     render(<LoginButtonWithGoogle />);
 
     const loginButtonWithGoogle = screen.getByText('Entrar com o Google');
 
     fireEvent.click(loginButtonWithGoogle);
+
+    expect(pushMock).toHaveBeenCalledWith('/conversas');
   });
 });
