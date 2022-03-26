59af9542566475c9751a567c60c3808b4a54f1c4
luis <luis.passos013@gmail.com>
2022-03-26 16:22:16 -0300

    add stack in trocar-senha form

--- src/pages/trocar-senha.tsx
+++ src/pages/trocar-senha.tsx
@@ -1,3 +1,4 @@
+import { Stack } from '@chakra-ui/react';
 import { BackLink } from '../components/BackLink';
 import { Button } from '../components/Form/Button';
 import { CenterForm } from '../components/Form/CenterForm';
@@ -8,24 +9,24 @@ import { Input } from '../components/Form/Input';
 export default function changePassword() {
   return (
     <CenterForm>
-      <FormTitle text='Trocar sua senha' />
+      <FormTitle mb='1rem' text='Trocar sua senha' />
       <FormWrapper>
-        <Input
-          id='newPassword'
-          label='Nova senha'
-          placeholder='Coloque sua nova senha...'
-        />
-
-        <Input
-          id='confirmNewPassword'
-          label='Confirme sua senha'
-          placeholder='Confirme sua nova senha...'
-        />
-
-        <Button text='REDEFINIR' />
+        <Stack spacing='.3rem'>
+          <Input
+            id='newPassword'
+            label='Nova senha'
+            placeholder='Coloque sua nova senha...'
+          />
+          <Input
+            id='confirmNewPassword'
+            label='Confirme sua senha'
+            placeholder='Confirme sua nova senha...'
+          />
+          <Button text='REDEFINIR' />
+        </Stack>
       </FormWrapper>
 
-      <BackLink text='Voltar' route='/' />
+      <BackLink text='Voltar' route='/' mt='1rem' />
     </CenterForm>
   );
 }
