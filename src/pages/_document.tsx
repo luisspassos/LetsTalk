72849b9dd243905bf5adba6c7f42f1bf2fcc15fe
Luís Passos <luis.passos013@gmail.com>
2022-03-27 20:23:03 -0300

    add html in cadastro page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
             crossOrigin='anonymous'
           />
           <link
-            href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto&display=swap'
+            href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Roboto&display=swap'
             rel='stylesheet'
           />
         </Head>
--- src/pages/cadastro.tsx
+++ src/pages/cadastro.tsx
@@ -1,3 +1,40 @@
+import { Header } from '../components/Header';
+import { Flex, Stack, Heading, Text } from '@chakra-ui/react';
+import { BackLink } from '../components/BackLink';
+import { FormWrapper } from '../components/Form/FormWrapper';
+import { Input } from '../components/Form/Input';
+import { Button } from '../components/Form/Button';
+
 export default function Register() {
-  return <h1>Cadastro</h1>;
+  return (
+    <>
+      <Header />
+      <Flex>
+        <Stack color='blue.900'>
+          <Heading as='h1'>
+            Mais de 200 mil usuários já estão conversando!
+          </Heading>
+          <Text>Junte-se e converse com outras pessoas!</Text>
+          <BackLink text='Fazer login' route='/' />
+        </Stack>
+        <FormWrapper>
+          <Input label='Email' id='email' type='email' placeholder='Email...' />
+          <Input label='Nome' id='username' placeholder='Nome...' />
+          <Input
+            label='Senha'
+            id='password'
+            type='password'
+            placeholder='Senha...'
+          />
+          <Input
+            label='Confirmar senha'
+            id='confirmPassword'
+            type='password'
+            placeholder='Confirme sua senha...'
+          />
+          <Button text='CADASTRAR' type='submit' />
+        </FormWrapper>
+      </Flex>
+    </>
+  );
 }
