df6c84ad41d5bfc9905d75218c4c81b789f57383
Luís Passos <luis.passos013@gmail.com>
2022-05-01 09:15:21 -0300

    feat: add button stack in account configurations

--- src/components/Configurations/index.tsx
+++ src/components/Configurations/index.tsx
@@ -1,13 +1,20 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Icon, Select } from '@chakra-ui/react';
 import { Divider } from '../Divider';
 import { PageTitle } from '../PageTitle';
 import { Avatar } from './Avatar';
 import { Box } from './Box';
 import { Button } from './Button';
 import { FaPencilAlt } from 'react-icons/fa';
-import { RiImageEditFill } from 'react-icons/ri';
+import {
+  RiDeleteBin2Line,
+  RiImageEditFill,
+  RiLockPasswordLine,
+} from 'react-icons/ri';
 import { FiShare2 } from 'react-icons/fi';
 import { ButtonStack } from './ButtonStack';
+import { HiOutlineMail } from 'react-icons/hi';
+import { ImExit } from 'react-icons/im';
+import { BiSun } from 'react-icons/bi';
 
 export function Configurations() {
   return (
@@ -24,11 +31,18 @@ export function Configurations() {
           </ButtonStack>
         </Box>
         <Box title='Configurações de conta'>
+          <Select>
+            <option value=''>
+              <Icon as={BiSun} />
+              <strong>Claro</strong>
+            </option>
+            <option value=''>Escuro</option>
+          </Select>
           <ButtonStack>
-            <Button text='Trocar email' leftIcon={FaPencilAlt} />
-            <Button text='Trocar senha' leftIcon={RiImageEditFill} />
-            <Button text='Deletar conta' leftIcon={FiShare2} />
-            <Button text='Sair' leftIcon={FiShare2} />
+            <Button text='Trocar email' leftIcon={HiOutlineMail} />
+            <Button text='Trocar senha' leftIcon={RiLockPasswordLine} />
+            <Button text='Deletar conta' leftIcon={RiDeleteBin2Line} />
+            <Button text='Sair' leftIcon={ImExit} />
           </ButtonStack>
         </Box>
       </Flex>
