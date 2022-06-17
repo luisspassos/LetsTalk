fe87739965a66e49a5a30084339ea2f100eeccf6
Luís Passos <luis.passos013@gmail.com>
2022-06-17 19:03:54 -0300

    feat: some codes have been commented out for testing in conversas.tsx

--- src/pages/conversas.tsx
+++ src/pages/conversas.tsx
@@ -1,9 +1,7 @@
 import { Flex } from '@chakra-ui/react';
-import { doc, onSnapshot } from 'firebase/firestore';
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
-import { useRouter } from 'next/router';
 import nookies from 'nookies';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Configurations } from '../components/Configurations';
 import { Conversations } from '../components/Conversations';
 import { Sidebar } from '../components/Sidebar';
@@ -12,11 +10,7 @@ import {
   ConversationsType,
   useConversations,
 } from '../contexts/ConversationsContext';
-import { useDeleteAccountModal } from '../contexts/Modal/DeleteAccountModalContext';
-import { useOnlineAtEvents } from '../contexts/OnlineAtEventsContext';
-import { useRenamingName } from '../contexts/RenamingNameContext';
 import { useTab } from '../contexts/TabContext';
-import { db } from '../services/firebase';
 import { auth as adminAuth } from '../services/firebaseAdmin';
 import { getConversations } from '../utils/getConversations';
 
@@ -27,29 +21,29 @@ type ConversationsPageProps = {
   timestamp: number;
 };
 
-function createLocationchangeEvent() {
-  let oldPushState = history.pushState;
-  history.pushState = function pushState() {
-    const args: any = arguments;
-    let ret = oldPushState.apply(this, args);
-    window.dispatchEvent(new Event('pushstate'));
-    window.dispatchEvent(new Event('locationchange'));
-    return ret;
-  };
+// function createLocationchangeEvent() {
+//   let oldPushState = history.pushState;
+//   history.pushState = function pushState() {
+//     const args: any = arguments;
+//     let ret = oldPushState.apply(this, args);
+//     window.dispatchEvent(new Event('pushstate'));
+//     window.dispatchEvent(new Event('locationchange'));
+//     return ret;
+//   };
 
-  let oldReplaceState = history.replaceState;
-  history.replaceState = function replaceState() {
-    const args: any = arguments;
-    let ret = oldReplaceState.apply(this, args);
-    window.dispatchEvent(new Event('replacestate'));
-    window.dispatchEvent(new Event('locationchange'));
-    return ret;
-  };
+//   let oldReplaceState = history.replaceState;
+//   history.replaceState = function replaceState() {
+//     const args: any = arguments;
+//     let ret = oldReplaceState.apply(this, args);
+//     window.dispatchEvent(new Event('replacestate'));
+//     window.dispatchEvent(new Event('locationchange'));
+//     return ret;
+//   };
 
-  window.addEventListener('popstate', () => {
-    window.dispatchEvent(new Event('locationchange'));
-  });
-}
+//   window.addEventListener('popstate', () => {
+//     window.dispatchEvent(new Event('locationchange'));
+//   });
+// }
 
 export default function ConversationsPage({
   user,
@@ -57,18 +51,18 @@ export default function ConversationsPage({
   token,
   timestamp,
 }: ConversationsPageProps) {
-  const { tab, handleChangeTab } = useTab();
-  const { fillUser, addUsernameInDb, user: contextUser } = useAuth();
+  const { tab } = useTab();
+  const { fillUser } = useAuth();
   const {
     conversations: { setConversations },
   } = useConversations();
-  const { takeUserOffline, takeUserOnline, setUserOnlineAt, clearAllEvents } =
-    useOnlineAtEvents();
-  const router = useRouter();
-  const { onClose: closeDeleteAccModal } = useDeleteAccountModal();
-  const { renamingName } = useRenamingName();
+  // const { takeUserOffline, takeUserOnline, setUserOnlineAt, clearAllEvents } =
+  //   useOnlineAtEvents();
+  // const router = useRouter();
+  // const { onClose: closeDeleteAccModal } = useDeleteAccountModal();
+  // const { renamingName } = useRenamingName();
 
-  const [ignoreAddingUserInDb, setIgnoreAddingUserInDb] = useState(false);
+  // const [ignoreAddingUserInDb, setIgnoreAddingUserInDb] = useState(false);
 
   useEffect(() => {
     console.log(timestamp);
@@ -77,76 +71,76 @@ export default function ConversationsPage({
     setConversations(conversations);
   }, [conversations, fillUser, setConversations, user, token, timestamp]);
 
-  useEffect(() => {
-    function checkIfUserAccHasBeenDeleted() {
-      if (!contextUser) return;
+  // useEffect(() => {
+  //   function checkIfUserAccHasBeenDeleted() {
+  //     if (!contextUser) return;
 
-      let ignoreInitialOnSnapshot = true;
+  //     let ignoreInitialOnSnapshot = true;
 
-      const unsub = onSnapshot(
-        doc(db, 'users', contextUser.username),
-        async (doc) => {
-          if (ignoreInitialOnSnapshot) {
-            ignoreInitialOnSnapshot = false;
-            return;
-          }
-          if (!doc.exists() && !renamingName) {
-            nookies.destroy({}, 'token');
-            clearAllEvents();
-            await router.push('/');
-            fillUser(null);
-            closeDeleteAccModal();
-            handleChangeTab('conversations');
-          }
-        }
-      );
+  //     const unsub = onSnapshot(
+  //       doc(db, 'users', contextUser.username),
+  //       async (doc) => {
+  //         if (ignoreInitialOnSnapshot) {
+  //           ignoreInitialOnSnapshot = false;
+  //           return;
+  //         }
+  //         if (!doc.exists() && !renamingName) {
+  //           nookies.destroy({}, 'token');
+  //           clearAllEvents();
+  //           await router.push('/');
+  //           fillUser(null);
+  //           closeDeleteAccModal();
+  //           handleChangeTab('conversations');
+  //         }
+  //       }
+  //     );
 
-      return unsub;
-    }
+  //     return unsub;
+  //   }
 
-    const unsub = checkIfUserAccHasBeenDeleted();
+  //   const unsub = checkIfUserAccHasBeenDeleted();
 
-    return () => {
-      unsub && unsub();
-    };
-  }, [
-    contextUser,
-    router,
-    clearAllEvents,
-    fillUser,
-    closeDeleteAccModal,
-    handleChangeTab,
-    renamingName,
-  ]);
+  //   return () => {
+  //     unsub && unsub();
+  //   };
+  // }, [
+  //   contextUser,
+  //   router,
+  //   clearAllEvents,
+  //   fillUser,
+  //   closeDeleteAccModal,
+  //   handleChangeTab,
+  //   renamingName,
+  // ]);
 
-  useEffect(() => {
-    (async () => {
-      // it will only add the user in the db when starting the application
+  // useEffect(() => {
+  //   (async () => {
+  //     // it will only add the user in the db when starting the application
 
-      if (!ignoreAddingUserInDb && user) {
-        await addUsernameInDb(user.username, user.uid);
-        setIgnoreAddingUserInDb(true);
-      }
+  //     if (!ignoreAddingUserInDb && user) {
+  //       await addUsernameInDb(user.username, user.uid);
+  //       setIgnoreAddingUserInDb(true);
+  //     }
 
-      setUserOnlineAt('now');
-    })();
-  }, [addUsernameInDb, ignoreAddingUserInDb, setUserOnlineAt, user]);
+  //     setUserOnlineAt('now');
+  //   })();
+  // }, [addUsernameInDb, ignoreAddingUserInDb, setUserOnlineAt, user]);
 
-  useEffect(() => {
-    createLocationchangeEvent();
+  // useEffect(() => {
+  //   createLocationchangeEvent();
 
-    for (let event of takeUserOffline.events) {
-      window.addEventListener(event, takeUserOffline.func);
-    }
+  //   for (let event of takeUserOffline.events) {
+  //     window.addEventListener(event, takeUserOffline.func);
+  //   }
 
-    for (let event of takeUserOnline.events) {
-      window.addEventListener(event, takeUserOnline.func);
-    }
+  //   for (let event of takeUserOnline.events) {
+  //     window.addEventListener(event, takeUserOnline.func);
+  //   }
 
-    return () => {
-      clearAllEvents();
-    };
-  }, [takeUserOnline, takeUserOffline, clearAllEvents]);
+  //   return () => {
+  //     clearAllEvents();
+  //   };
+  // }, [takeUserOnline, takeUserOffline, clearAllEvents]);
 
   const CurrentTab = {
     conversations: Conversations,
@@ -166,11 +160,6 @@ export default function ConversationsPage({
 export const getServerSideProps: GetServerSideProps = async (
   ctx: GetServerSidePropsContext
 ) => {
-  ctx.res.setHeader(
-    'Cache-Control',
-    'no-cache, no-store, max-age=0, must-revalidate'
-  );
-
   try {
     const cookies = nookies.get(ctx);
     const user = await adminAuth.verifyIdToken(cookies.token);
