874a6b2c23a5f1582816d5e5f3630fe6fbbde79c
Luís Passos <luis.passos013@gmail.com>
2022-06-17 19:25:49 -0300

    feat: added test route

---
+++ src/pages/test.tsx
@@ -0,0 +1,41 @@
+import { Button, Heading } from '@chakra-ui/react';
+import { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+type TestProps = {
+  timestamp: number;
+};
+
+export default function Test({ timestamp }: TestProps) {
+  const router = useRouter();
+
+  async function handle() {
+    const { auth } = await import('../services/firebase');
+    const { signOut } = await import('firebase/auth');
+
+    await signOut(auth);
+    await router.push('/');
+  }
+
+  useEffect(() => {
+    console.log(timestamp);
+  }, [timestamp]);
+
+  return (
+    <>
+      <Heading>Test</Heading>
+      <Button onClick={handle} colorScheme='pink'>
+        Voltar para a home
+      </Button>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      timestamp: Date.now(),
+    },
+  };
+};
