db44c1fd3f7f06b384dbc050c37d925f47299240
Luís Passos <luis.passos013@gmail.com>
2022-11-28 18:26:59 -0300

    refactor(virtualizer/dynamic/item): remove some comments

--- src/components/Virtualizer/Dynamic/Item.tsx
+++ src/components/Virtualizer/Dynamic/Item.tsx
@@ -15,27 +15,10 @@ type ItemProps = {
   children: ReactNode;
 } & DivProps;
 
-// export function Item({ start, children, style, ...rest }: ItemProps) {
-//   return (
-//     // chakra element isn't being used to perform the list
-//     <div
-//       style={{
-//         position: 'absolute',
-//         top: 0,
-//         left: 0,
-//         width: '100%',
-//         transform: `translateY(${start}px)`,
-//         ...style,
-//       }}
-//       {...rest}
-//     >
-//       {children}
-//     </div>
-//   );
-// }
-
 export const Item = forwardRef<HTMLDivElement, ItemProps>(
   ({ start, style, children, ...rest }, ref) => (
+    // chakra element isn't being used to perform the list
+
     <div
       ref={ref}
       style={{
