21941504203ca9f7ed4fc20c94c2b9d609de4fbf
luis <luis.passos013@gmail.com>
2022-12-10 15:12:17 -0300

    refactor(theme.ts): remove withDefaultProps

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,4 @@
-import {
-  ColorMode,
-  extendTheme,
-  ThemeConfig,
-  withDefaultProps,
-} from '@chakra-ui/react';
+import { ColorMode, extendTheme, ThemeConfig } from '@chakra-ui/react';
 import { colors } from './colors';
 
 type GlobalStyleProps = {
@@ -17,52 +12,44 @@ export const themeConfig: ThemeConfig = {
 
 const boxShadow = `0 0 0 3px ${colors.blueAlpha[400]}`;
 
-export const theme = extendTheme(
-  withDefaultProps({
-    defaultProps: {
-      isLazy: true,
-    },
-    components: ['Menu', 'Popover'],
-  }),
-  {
-    fonts: {
-      heading: 'Poppins, Noto Color Emoji, sans-serif',
-      body: 'Poppins, Noto Color Emoji, sans-serif',
-    },
-    shadows: {
-      outline: boxShadow,
-      'inner-blue': `inset ${boxShadow}`,
-    },
-    colors,
-    styles: {
-      global: (props: GlobalStyleProps) => {
-        const isDark = props.colorMode === 'dark';
+export const theme = extendTheme({
+  fonts: {
+    heading: 'Poppins, Noto Color Emoji, sans-serif',
+    body: 'Poppins, Noto Color Emoji, sans-serif',
+  },
+  shadows: {
+    outline: boxShadow,
+    'inner-blue': `inset ${boxShadow}`,
+  },
+  colors,
+  styles: {
+    global: (props: GlobalStyleProps) => {
+      const isDark = props.colorMode === 'dark';
 
-        const placeholderColor = isDark ? 'whiteAlpha.800' : 'blackAlpha.700';
+      const placeholderColor = isDark ? 'whiteAlpha.800' : 'blackAlpha.700';
 
-        return {
-          '*': {
-            wordBreak: 'break-word',
-          },
-          body: {
-            bg: isDark ? 'blue.900' : 'gray.50',
-            color: isDark ? 'gray.50' : 'gray.900',
-          },
-          '*::placeholder': {
-            color: placeholderColor,
-          },
-          '::-webkit-scrollbar': {
-            width: ['3px', '6px'],
-          },
-          '::-webkit-scrollbar-thumb': {
-            bgColor: isDark ? colors.scrollbar.dark : colors.scrollbar.light,
-          },
-          '#chakra-toast-manager-top-right': {
-            alignItems: 'end',
-          },
-        };
-      },
+      return {
+        '*': {
+          wordBreak: 'break-word',
+        },
+        body: {
+          bg: isDark ? 'blue.900' : 'gray.50',
+          color: isDark ? 'gray.50' : 'gray.900',
+        },
+        '*::placeholder': {
+          color: placeholderColor,
+        },
+        '::-webkit-scrollbar': {
+          width: ['3px', '6px'],
+        },
+        '::-webkit-scrollbar-thumb': {
+          bgColor: isDark ? colors.scrollbar.dark : colors.scrollbar.light,
+        },
+        '#chakra-toast-manager-top-right': {
+          alignItems: 'end',
+        },
+      };
     },
-    ...themeConfig,
-  }
-);
+  },
+  ...themeConfig,
+});
