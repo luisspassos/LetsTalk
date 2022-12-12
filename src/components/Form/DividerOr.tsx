1fd919440b0d9a99d772edf8dbedb64b82ebcdb5
Luís Passos <luis.passos013@gmail.com>
2022-12-12 17:52:12 -0300

    refactor(form/divideror): add const line

--- src/components/Form/DividerOr.tsx
+++ src/components/Form/DividerOr.tsx
@@ -1,6 +1,15 @@
 import { Flex, useColorModeValue } from '@chakra-ui/react';
 
 export function DividerOr() {
+  const line = {
+    content: '""',
+    bg: useColorModeValue('gray.400', 'gray.50'),
+    h: '1px',
+    w: '100%',
+    d: 'block',
+    opacity: '80%',
+  };
+
   return (
     <Flex
       align='center'
@@ -9,22 +18,8 @@ export function DividerOr() {
       my={['10px', '15px']}
       fontSize={['16px', '18px']}
       wordBreak='normal'
-      _before={{
-        content: '""',
-        bg: useColorModeValue('gray.400', 'gray.50'),
-        h: '1px',
-        w: '100%',
-        d: 'block',
-        opacity: '80%',
-      }}
-      _after={{
-        content: '""',
-        bg: useColorModeValue('gray.400', 'gray.50'),
-        h: '1px',
-        w: '100%',
-        d: 'block',
-        opacity: '80%',
-      }}
+      _before={line}
+      _after={line}
     >
       ou
     </Flex>
