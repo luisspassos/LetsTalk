1fa82f9661e046cb922d00f5ff6410de404d3a41
luis <luis.passos013@gmail.com>
2022-12-30 21:52:19 -0300

    feat(avatar): testing fallback img

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,57 +1,49 @@
-import { ImageProps, Box, Img } from '@chakra-ui/react';
+import { ImageProps, Img, Skeleton } from '@chakra-ui/react';
 import { useState } from 'react';
 
 type AvatarProps = Omit<ImageProps, 'src'> & {
   src: string | null | undefined;
 };
 
-export function Avatar({ src, ...props }: AvatarProps) {
+export function Avatar({ src: srcProp, ...props }: AvatarProps) {
+  const abstractUserIcon = 'images/abstract-user.svg';
+
   const [isLoaded, setIsLoaded] = useState(false);
+
+  const [src, setSrc] = useState(srcProp ?? abstractUserIcon);
 
   function handleLoad() {
     setIsLoaded(true);
   }
 
+  function handleError() {
+    setSrc(abstractUserIcon);
+    setIsLoaded(true);
+  }
+
   const styles = {
-    w: '12',
-    h: '12',
     borderRadius: '50%',
-    boxShadow: 'sm',
   };
 
   return (
-    <Box position='relative'>
-      {/* {!isLoaded && (
-        <Flex
-          {...styles}
-          {...props}
-          direction='column'
-          align='center'
-          bg='gray.300'
-          shrink={0}
-          position='absolute'
-          overflow='hidden'
-        >
-          <Box mt='20%' bg='white' w='35%' h='35%' borderRadius='50%' />
-          <Box
-            bg='white'
-            w='75%'
-            h='75%'
-            borderRadius='50%'
-            bottom='-35%'
-            pos='absolute'
-          />
-        </Flex>
-      )} */}
-
+    <Skeleton
+      isLoaded={isLoaded}
+      startColor='gray.300'
+      endColor='gray.400'
+      {...props}
+      {...styles}
+    >
       <Img
         onLoad={handleLoad}
-        src='images/abstract-user.svg'
+        onError={handleError}
+        src={src}
         alt='Imagem de perfil'
-        opacity={isLoaded ? 1 : 0}
+        w='12'
+        h='12'
+        boxShadow='sm'
         {...styles}
         {...props}
       />
-    </Box>
+    </Skeleton>
   );
 }
