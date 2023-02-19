369f4c181f74ce93ca686c8f61dce139570912bf
luis <luis.passos013@gmail.com>
2023-02-19 14:59:47 -0300

    feat(side bar): responsive styles

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -6,26 +6,33 @@ import { ConversationsButton } from './Buttons/ConversationsButton';
 
 export function Sidebar() {
   return (
-    <Flex
+    <Box
       bg={useColorModeValue('gray.500', 'blue.900')}
-      direction='column'
-      justify='space-between'
-      align='center'
-      py='1.8%'
-      px='1%'
       w='5.5%'
-      borderRight={useColorModeValue('unset', '1px solid')}
-      borderRightColor={useColorModeValue('unset', 'whiteAlpha.500')}
+      minW='4.0625rem'
+      maxW='5.25rem'
       as='nav'
     >
-      <Box>
-        <Avatar />
-        <VStack spacing='32%' mt='64%'>
-          <ConversationsButton />
-          <ConfigurationsButton />
-        </VStack>
-      </Box>
-      <SignOutButton />
-    </Flex>
+      <Flex
+        direction='column'
+        justify='space-between'
+        align='center'
+        w='100%'
+        h='100%'
+        borderRight={useColorModeValue('unset', '1px solid')}
+        borderRightColor={useColorModeValue('unset', 'whiteAlpha.500')}
+        px='17%'
+        py='25%'
+      >
+        <Box>
+          <Avatar />
+          <VStack spacing='32%' mt='64%'>
+            <ConversationsButton />
+            <ConfigurationsButton />
+          </VStack>
+        </Box>
+        <SignOutButton />
+      </Flex>
+    </Box>
   );
 }
