c140711eb4b32237bace5ee040a575dbb067f1af
Luís Passos <luis.passos013@gmail.com>
2023-03-12 16:56:40 -0300

    refactor(sidebar small screen): using reusable components

--- src/components/ConversationsPage/Sidebars/SmallScreen/Sidebar/index.tsx
+++ src/components/ConversationsPage/Sidebars/SmallScreen/Sidebar/index.tsx
@@ -1,10 +1,9 @@
 import { Flex } from '@chakra-ui/react';
 import { useMobileConversationListDividerColor } from 'hooks/Colors/useMobileConversationListDividerColor';
-import { ConfigurationsButton } from './Buttons/ConfigurationsButton';
-import { ConversationsButton } from './Buttons/ConversationsButton';
 import { Avatar } from './Avatar';
 import { SignOutButton } from './Buttons/SignOutButton';
 import { Wrapper } from 'components/SidebarBase/Wrapper';
+import { RouteButtons } from './Buttons/RouteButtons';
 
 export function Sidebar() {
   const { color: borderColor } = useMobileConversationListDividerColor();
@@ -22,8 +21,7 @@ export function Sidebar() {
       <Flex h='100%' align='center' w='100%'>
         <Avatar />
         <Flex flex='1' h='100%' align='center' ml='1.2em' gap='.3em'>
-          <ConversationsButton />
-          <ConfigurationsButton />
+          <RouteButtons />
         </Flex>
       </Flex>
       <SignOutButton />
