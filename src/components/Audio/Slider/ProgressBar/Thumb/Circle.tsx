7d30895c2c12baa2934984e9893a0d627c010253
Luís Passos <luis.passos013@gmail.com>
2023-04-23 18:02:00 -0300

    feat(audio/thumb/circle): added aria attributes and styles for accessibility

--- src/components/Audio/Slider/ProgressBar/Thumb/Circle.tsx
+++ src/components/Audio/Slider/ProgressBar/Thumb/Circle.tsx
@@ -3,7 +3,11 @@ import { Box } from '@chakra-ui/react';
 export function Circle() {
   return (
     <Box
+      role='slider'
+      aria-orientation='horizontal'
+      aria-label='Controle de progresso do áudio'
       h='100%'
+      tabIndex={0}
       bg='current'
       borderRadius='50%'
       pos='absolute'
@@ -11,10 +15,14 @@ export function Circle() {
       cursor='pointer'
       transform='translateX(50%)'
       pointerEvents='none'
+      outline={0}
       sx={{
         '&': {
           aspectRatio: '1 / 1',
         },
+      }}
+      _focusVisible={{
+        boxShadow: 'outline',
       }}
     />
   );
