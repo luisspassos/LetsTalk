a268737fb14328eb31b4e1358d654be00ebdf803
Luís Passos <92176205+luisspassos@users.noreply.github.com>
2023-05-13 20:55:43 -0300

    test(cypress/e2e/pages/404.cy.ts): should go to the login page when clicking the back link

--- cypress/e2e/pages/404.cy.ts
+++ cypress/e2e/pages/404.cy.ts
@@ -1,3 +1,11 @@
 describe('404 Page', () => {
-  it('successfully loads', () => {});
+  it('should go to the login page when clicking the back link', () => {
+    cy.visit('/404', { failOnStatusCode: false });
+
+    cy.contains('Voltar').click();
+
+    cy.contains('Entrar com o Google');
+  });
 });
+
+export {};
