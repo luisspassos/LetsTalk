00b883be7edfc1dfaa6222d79f6dd350a7e5dea3
Luís Passos <92176205+luisspassos@users.noreply.github.com>
2023-06-01 21:50:01 -0300

    test(cypress/pages/index.cy.ts): add 'should open google sign in popup' test

--- cypress/e2e/pages/index.cy.ts
+++ cypress/e2e/pages/index.cy.ts
@@ -1,20 +1,17 @@
 describe('Login page', () => {
-  beforeEach(() => {});
+  // beforeEach(() => {
+  //   cy.visit('/');
+  // });
 
-  it('should sign in with google', () => {
+  it('should open google sign in popup', () => {
+    cy.visit('/', {
+      onBeforeLoad(win) {
+        cy.stub(win, 'open');
+      },
+    });
+
     cy.get('[data-cy="google login button"]').click();
-    cy.contains('Conversas');
+
+    cy.window().its('open').should('be.called');
   });
-
-  // it('sign up button should redirect to sign up page', () => {
-  //   cy.contains('Cadastre-se').click();
-
-  //   cy.get('button').contains('CADASTRAR');
-  // });
-
-  // it('forgot my password should redirect to forgot my password page', () => {
-  //   cy.contains('Esqueci minha senha').click();
-
-  //   cy.get('h1').contains('Envie seu email para recuperar sua senha');
-  // });
 });
