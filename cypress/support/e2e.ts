de8da4da6d75c9173c4ad1925db26be6bb401fe7
Luís Passos <92176205+luisspassos@users.noreply.github.com>
2023-06-03 16:33:02 -0300

    test(cypress): add cypress-firebase config

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,1 +1,12 @@
 import './commands';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+import 'firebase/compat/firestore';
+import { attachCustomCommands } from 'cypress-firebase';
+import { firebaseConfig } from '../../src/services/firebase';
+
+firebase.initializeApp(firebaseConfig);
+
+attachCustomCommands({ Cypress, cy, firebase });
