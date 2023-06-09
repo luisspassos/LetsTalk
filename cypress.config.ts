39bffd585fa13772e0561e8f58e05abd4b4cdb6c
Luís Passos <92176205+luisspassos@users.noreply.github.com>
2023-06-09 15:06:01 -0300

    chore(cypress.config.ts): fix typescript errors

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'cypress';
 import { plugin as cypressFirebasePlugin } from 'cypress-firebase';
+
+// @ts-ignore: This module is declared with using 'export =', and can only be used with a default import when using the 'esModuleInterop' flag.
 import admin from 'firebase-admin';
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    // @ts-ignore: Property 'projectRoot' is optional in type 'ExtendedCypressConfig' but required in type 'PluginConfigOptions'.
     setupNodeEvents(on, config) {
       on('before:browser:launch', (_, launchOptions) => {
         launchOptions.args.push('--enable-features=SharedArrayBuffer');
