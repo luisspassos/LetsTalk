83e0a221d8e602b31dc5fd67663b4a0f08c9c306
Luís Passos <92176205+luisspassos@users.noreply.github.com>
2023-06-08 21:15:42 -0300

    test(cypress): remove unused code

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,26 +2,10 @@ import { defineConfig } from 'cypress';
 import { plugin as cypressFirebasePlugin } from 'cypress-firebase';
 import admin from 'firebase-admin';
 
-import webpackPreprocessor from '@cypress/webpack-preprocessor';
-
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      const options = webpackPreprocessor.defaultOptions;
-
-      options.webpackOptions?.module?.rules?.[0].use[0].options.plugins = [
-        ['@babel/plugin-transform-modules-commonjs', { loose: true }],
-      ];
-
-      options.webpackOptions?.resolve = {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      };
-
-      console.log(options);
-
-      on('file:preprocessor', webpackPreprocessor(options));
-
       on('before:browser:launch', (_, launchOptions) => {
         launchOptions.args.push('--enable-features=SharedArrayBuffer');
 
--- cypress/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-
-declare namespace Cypress {
-  interface Chainable<Subject = any> {
-    loginByGoogleApi(): Chainable<any>;
-  }
-}
