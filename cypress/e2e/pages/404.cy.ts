dcc24cc69f6249ece1d33e1c360e7930337f02b3
Luís Passos <92176205+luisspassos@users.noreply.github.com>
2023-07-03 18:05:35 -0300

    test(404 page): refactor links test

--- cypress/e2e/pages/404.cy.ts
+++ cypress/e2e/pages/404.cy.ts
@@ -1,10 +1,11 @@
 describe('404 Page', () => {
-  it('should go to the login page when clicking the back link', () => {
-    cy.visit('/404', { failOnStatusCode: false });
-
-    cy.contains('Voltar').click();
-
-    cy.contains('Entrar com o Google');
+  describe('links', () => {
+    beforeEach(() => {
+      cy.visit('/404', { failOnStatusCode: false });
+    });
+    it('back to the homepage', () => {
+      cy.testLink('login');
+    });
   });
 });
 
