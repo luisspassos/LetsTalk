338d459cb43d85b4c6781a78236c4537bc68af9d
Luís Passos <92176205+luisspassos@users.noreply.github.com>
2023-07-13 16:23:05 -0300

    chore(firebase): remove emulators

--- src/tests/support/e2e/index.ts
+++ src/tests/support/e2e/index.ts
@@ -11,20 +11,6 @@ import { firebaseConfig } from 'services/firebase';
 
 firebase.initializeApp(firebaseConfig);
 
-const firestoreEmulatorHost = Cypress.env('FIRESTORE_EMULATOR_HOST');
-if (firestoreEmulatorHost) {
-  firebase.firestore().settings({
-    host: firestoreEmulatorHost,
-    ssl: false,
-  });
-}
-
-const authEmulatorHost = Cypress.env('FIREBASE_AUTH_EMULATOR_HOST');
-if (authEmulatorHost) {
-  firebase.auth().useEmulator(`http://${authEmulatorHost}/`);
-  console.debug(`Using Auth emulator: http://${authEmulatorHost}/`);
-}
-
 attachCustomCommands({ Cypress, cy, firebase });
 
 // Uncaught Exceptions
